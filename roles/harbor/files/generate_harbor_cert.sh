e6d98fc025fc06782632c20c7197e3eb6b967c60
2kk <80165051@qq.com>
2021-04-02 11:38:36 +0800

    harbor安装修正语法错误

--- roles/harbor/files/generate_harbor_cert.sh
+++ roles/harbor/files/generate_harbor_cert.sh
@@ -1,8 +1,8 @@
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes harbor-csr.json cfssljson -bare harbor
 # 拷贝到使用证书的roles下
-root_dir=$(pwd |sed 's#ssl/harbor##')
-harbor_cert_dir=$root_dir/roles/harbor/files/harbor_cert
+root_dir=$(pwd)
+harbor_cert_dir=$root_dir/harbor/harbor_cert
 mkdir -p $harbor_cert_dir
 for dir in $harbor_cert_dir ; do
    cp -rf ca.pem server.pem server-key.pem $dir
