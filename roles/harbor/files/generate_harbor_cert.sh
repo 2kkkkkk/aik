f97c192a59130cd34a683b99cfb095f13483e602
2kk <80165051@qq.com>
2021-04-02 13:59:08 +0800

    ls

--- roles/harbor/files/generate_harbor_cert.sh
+++ roles/harbor/files/generate_harbor_cert.sh
@@ -1,3 +1,3 @@
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
-cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes harbor-csr.json cfssljson -bare harbor
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes harbor-csr.json | cfssljson -bare harbor
 
