b4a3397da659f70bbad28abd5cd529de06c65863
2kk <80165051@qq.com>
2021-04-23 18:03:43 +0800

    v1.18

--- README.md
+++ README.md
@@ -1,47 +1,15 @@
-# Kubernetes 高可用集群自动部署 二进制 
->### 确保所有节点系统时间一致
-
+# Kubernetes 企业级高可用集群
+``` 
+支持的kubernetes版本：v1.18 v1.19
+```
 ### 1、找一台服务器安装Ansible
 ```
-# 需要安装python3 用pip3安装ansible
-#python3安装
-1.依赖包安装
-yum -y  install gcc-c++
-yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel libffi-devel
-2.下载包：
-#下载路径(里面有不同的版本)https://www.python.org/ftp/python/3.8.8/
-#下载命令
-wget https://www.python.org/ftp/python/3.8.8/Python-3.8.8.tar.xz
-3.解压：
-tar -zxvf Python-3.8.8.tar.xz
-4.安装：
-cd Python-3.8.8
-#指定编译目录
-mkdir /usr/local/python3
-./configure --prefix=/usr/local/python3 --with-ssl
-#编译安装
-make && make install
-5.建立软连接
-ln -s /usr/local/python3/bin/python3.8 /usr/bin/python3
-ln -s /usr/local/python3/bin/pip3.8 /usr/bin/pip3  没有pip直接就pip
-6.测试一下python3是否可以用
-python3
-pip3
-安装ansible
-pip install ansible -i https://mirrors.aliyun.com/pypi/simple/
-#ansible需要sshpass
-yum -y install sshpass
-#ansible配置文件
-https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
-#ansible需要sshpass
-yum -y install sshpass
-#软链接ansible-playbook
-ln -s /usr/local/python3/bin/ansible-playbook  /usr/bin/ansible-playbook
-#软链接
-ln -s /usr/local/python3/bin/ansible  /usr/bin/ansible
-# 安装netaddr模块
-pip install netaddr -i https://mirrors.aliyun.com/pypi/simple/
-
+#执行python3和ansible安装脚本
+/bin/bash install_py3.sh
+#执行内核更新脚本 （可选 内核选择ml）
+/bin/bash kernel_update.sh
+#执行系统初始化脚本 （可选 需要更改DNS信息）
+/bin/bash linux_initali.sh
 ```
 ### 2、下载所需文件
 
@@ -50,14 +18,11 @@ pip install netaddr -i https://mirrors.aliyun.com/pypi/simple/
 ```
 # git clone https://gitee.com/Zheng--Kai/aik.git
 # cd aik
-
 ```
 
 下载准备好软件包并解压/root目录：
 
-只对1.19.8进行安装测试
-
-链接: https://pan.baidu.com/s/1U-566cbdqXGOP7JEkB5Y1g
+链接: 链接：https://pan.baidu.com/s/1kkPAR0xFztB2yepe4t7yRg
 
 提取码: 2kkk
 
@@ -69,17 +34,43 @@ pip install netaddr -i https://mirrors.aliyun.com/pypi/simple/
 修改hosts文件，根据规划修改对应IP和名称。
 
 ```
-# vi hosts
+# vim hosts
 [master]
+# 如果部署单Master，只保留一个Master节点
+# 默认Naster节点也部署Node组件
 172.16.163.111 node_name=k8s-master1 hostname=k8s-master1
+172.16.163.112 node_name=k8s-master2 hostname=k8s-master2
+172.16.163.113 node_name=k8s-master3 hostname=k8s-master3
+
 [node]
 172.16.163.114 node_name=k8s-node1 hostname=k8s-node1
+172.16.163.115 node_name=k8s-node2 hostname=k8s-node2
+
 [etcd]
 172.16.163.111 etcd_name=etcd-1 hostname=k8s-master1
+172.16.163.112 etcd_name=etcd-2 hostname=k8s-master2
+172.16.163.113 etcd_name=etcd-3 hostname=k8s-master3
+
 [lb]
 # 如果部署单Master，该项忽略
 172.16.163.111 lb_name=lb-master hostname=k8s-master1
-#用于添加节点
+172.16.163.112 lb_name=lb-backup hostname=k8s-master2
+
+[k9s]
+#k9s:k8s 集群管理的工具,哪个IP要就写哪个
+172.16.163.100 hostname=k8s-client100
+172.16.163.111
+172.16.163.112
+172.16.163.113
+
+#harbor 仓库, docker私人仓库
+[harbor]
+#172.16.163.43 hostname=harbor
+
+[k8s:children]
+master
+node
+
 [newnode]
 #172.16.163.106 node_name=k8s-node3
 
@@ -87,12 +78,27 @@ pip install netaddr -i https://mirrors.aliyun.com/pypi/simple/
 ansible -i hosts all  -m shell -a "date" -uroot -k
 ...
 ```
-修改group_vars/all.yml文件，修改软件包目录和证书可信任IP。
-每段参数都有备注，注意ip是对的 网络保持一致
+修改group_vars/all.yml文件，修改软件包目录和容器引擎,网络插件方案还有证书可信任IP。
+其他参数都有备注。
 ```
 # vim group_vars/all.yml
+#K9s - Kubernetes CLI管理K8s集群,不许要安装设置为false
+k9s_install: true
+
+# 支持的集群容器引擎: docker, containerd
+container_runtime: docker
+
+# 选择网络插件方案 
+addons_Option_I: true
+addons_Option_II: false
+
+# 网络插件选择 calico flannel cilium
+network_plugin: calico
+
+#安装包目录
 software_dir: '/root/binary_pkg'
 ...
+#证书可信任ip
 cert_hosts:
   k8s:
   etcd:
@@ -105,15 +111,31 @@ cert_hosts:
 多Master架构
 ![avatar](https://images.gitee.com/uploads/images/2021/0225/170745_b7ba1da3_8721850.jpeg "multi-master.jpg")
 
+###Containerd容器引擎 
+
+多Master版：
+```
+# ansible-playbook -i hosts multi-master-containerd.yml -uroot -k
+```
 单Master版：
 ```
-# ansible-playbook -i hosts single-master-docker.yml -uroot -k
+# ansible-playbook -i hosts single-master-containerd.yml -uroot -k
 ```
+
+###Docker容器引擎 1.23版本弃用
+
 多Master版：
 ```
 # ansible-playbook -i hosts multi-master-docker.yml -uroot -k
 ```
-
+单Master版：
+```
+# ansible-playbook -i hosts single-master-docker.yml -uroot -k
+```
+###Harbor仓库安装
+```
+# ansible-playbook -i hosts harbor.yml -uroot -k
+```
 ## 5、部署控制
 如果安装某个阶段失败，可针对性测试.
 
@@ -126,13 +148,14 @@ cert_hosts:
 1）修改hosts，添加新节点ip
 ```
 # vim hosts
+[newnode]
+172.16.163.100 node_name=k8s-node3
 ```
 2）执行部署
 ```
 ansible-playbook -i hosts add-node.yml -uroot -k
 ```
-3）在Master节点允许颁发证书并加入集群
+## 7、所有HTTPS证书存放路径
 ```
-kubectl get csr
-kubectl certificate approve node-csr-xxx
+部署产生的证书都会存放到目录“aik/ssl”
 ```
--- install_py3.sh
+++ install_py3.sh
@@ -1,23 +1,91 @@
 #!/bin/bash
 
-munu(){
-cat << EOF
-*********MENU**********
- 1. ALL INSTALL
- 2. INSTALL Python
- 3. INSTALL Ansible
- 4. EXIT
-***********************
-EOF
+#############################################################################################
+# 导入系统变量
+#############################################################################################
+. /etc/init.d/functions
+source /etc/profile
+
+#############################################################################################
+# 服务变量定义
+#############################################################################################
+# 线条
+LINE='---------------------------------------------------------------------------------------'
+
+
+
+#############################################################################################
+# 颜色输出函数
+#############################################################################################
+function FUNC_COLOR_TEXT() {
+  echo -e " \e[0;$2m$1\e[0m"
+}
+
+function FUNC_ECHO_RED() {
+  echo $(FUNC_COLOR_TEXT "$1" "31")
+}
+
+function FUNC_ECHO_GREEN() {
+  echo $(FUNC_COLOR_TEXT "$1" "32")
+}
+
+function FUNC_ECHO_YELLOW() {
+  echo $(FUNC_COLOR_TEXT "$1" "33")
+}
+
+function FUNC_ECHO_BLUE() {
+  echo $(FUNC_COLOR_TEXT "$1" "34")
+}
+
+#############################################################################################
+# 颜色通知输出函数
+#############################################################################################
+# 通知信息
+function FUNC_ECHO_INFO() {
+  echo $(FUNC_COLOR_TEXT "${LINE}" "33")
+  echo $(FUNC_COLOR_TEXT "$1" "33")
+  echo $(FUNC_COLOR_TEXT "${LINE}" "33")
+}
+
+# 完成信息
+function FUNC_ECHO_SUCCESS() {
+  echo $(FUNC_COLOR_TEXT "${LINE}" "32")
+  echo $(FUNC_COLOR_TEXT "$1" "32")
+  echo $(FUNC_COLOR_TEXT "${LINE}" "32")
+}
+
+# 错误信息
+function FUNC_ECHO_ERROR() {
+  echo $(FUNC_COLOR_TEXT "${LINE}" "31")
+  echo $(FUNC_COLOR_TEXT "$1" "31")
+  echo $(FUNC_COLOR_TEXT "${LINE}" "31")
+}
+
+#############################################################################################
+# 菜单选择函数
+#############################################################################################
+function FUNC_MUNU(){
+echo -e "\033[33m*********MENU**********\033[0m"
+echo -e "\033[33m1. ALL INSTALL\033[0m"
+echo -e "\033[33m2. INSTALL Python\033[0m"
+echo -e "\033[33m3. INSTALL Ansible\033[0m"
+echo -e "\033[33m4. INSTALL K9S\033[0m"
+echo -e "\033[33m5. INSTALL Mycli\033[0m"
+echo -e "\033[33m0. EXIT\033[0m"
+echo -e "\033[33m***********************\033[0m"
 }
 
 py_pyth="/usr/local/python3"
 pak_v="Python-3.*"
 
-py3(){
+#############################################################################################
+# Python3安装函数
+#############################################################################################
+
+function FUNC_PY3(){
 #安装python3
 if [ ! -f $pak_v.tar.xz ];then
-	echo "未发现Python安装包，准备在线下载！"
+	FUNC_ECHO_INFO "未发现Python安装包，准备在线下载！"
 	/usr/bin/which wget >/dev/null 2>&1
 	if [ `echo $?` -ne 0 ];then
 		yum -y install wget >/dev/null 2>&1
@@ -34,51 +102,95 @@ if [ `echo $?` -eq 0 ];then
 	./configure --prefix=/usr/local/python3 --with-ssl 
 	make && make install
 	if [ `echo $?` -eq 0 ];then
-		ln -sf $py_pyth/bin/python3.8 /usr/bin/python3
-		ln -sf $py_pyth/bin/pip3.8 /usr/bin/pip3
-		echo "当前python安装版本: `/usr/bin/python3 --version`"
+		ln -sf $py_pyth/bin/python3.8 /usr/local/bin/python3
+		ln -sf $py_pyth/bin/pip3.8 /usr/local/bin/pip3
+		FUNC_ECHO_SUCCESS "当前python安装版本: `/usr/local/bin/python3 --version`"
 	else
-		echo -n "编译安装失败，退出码："
+		FUNC_ECHO_ERROR -n "编译安装失败，退出码："
 		exit 2
+	fi
+fi
+
+
+}
+
+
+function FUNC_MYCLI(){
+which mycli >/dev/null 2>&1
+if [ `echo $?` -ne 0 ];then
+	FUNC_ECHO_INFO "开始安装Mycli！"
+	/usr/local/bin/pip3 install mycli -i https://mirrors.aliyun.com/pypi/simple/
+	if [ `echo $?` -eq 0 ];then
+		ln -s /usr/local/python3/bin/mycli  /usr/local/bin/mycli
 	fi
 fi
 }
 
+function FUNC_K9S(){
+k9s version > /dev/null 2>&1
+if [ `echo $?` -ne 0 ];then
+	if [ ! -f k9s_Linux_x86_64.tar.gz ];then
+		FUNC_ECHO_INFO "准备下载K9S..."
+		wget https://github.com.cnpmjs.org/derailed/k9s/releases/download/v0.24.6/k9s_Linux_x86_64.tar.gz > /dev/null 2>&1
+		if [ `echo $?` -ne 0 ];then
+			FUNC_ECHO_ERROR "下载失败，将退出"
+			break
+		fi
+	fi
+	tar zxf k9s_Linux_x86_64.tar.gz/k9s -C /usr/local/bin
+	chmod +x /usr/local/bin/k9s
+	FUNC_ECHO_SUCCESS " 当前K9S安装版本为： `/usr/local/bin/k9s version|grep 'Version:'`"
+fi
 
-asb(){
+
+}
+
+function FUNC_ANSIBLE(){
 #安装ansible
 /usr/bin/which ansible >/dev/null 2>&1
 if [ `echo $?` -eq 0 ];then
-	echo "当前ansible已安装版本是：`ansible --version|head -n 1`"
+	FUNC_ECHO_SUCCESS "当前ansible已安装版本是：`/usr/local/bin/ansible --version|head -n 1`"
 else
-	/usr/bin/pip3 install ansible -i https://mirrors.aliyun.com/pypi/simple/
-	/usr/bin/pip3 install netaddr -i https://mirrors.aliyun.com/pypi/simple/
+	FUNC_ECHO_INFO "开始安装ansible..."
+	/usr/local/bin/pip3 install ansible -i https://mirrors.aliyun.com/pypi/simple/
+	/usr/local/bin/pip3 install netaddr -i https://mirrors.aliyun.com/pypi/simple/
 	yum -y install sshpass
-	ln -s /usr/local/python3/bin/ansible-playbook  /usr/bin/ansible-playbook
-	ln -s /usr/local/python3/bin/ansible  /usr/bin/ansible
-	echo "当前ansible安装版本是：`ansible --version|head -n 1`"
+	ln -s /usr/local/python3/bin/ansible-playbook  /usr/local/bin/ansible-playbook
+	ln -s /usr/local/python3/bin/ansible  /usr/local/bin/ansible
+	FUNC_ECHO_SUCCESS "当前ansible安装版本是：`/usr/local/bin/ansible --version|head -n 1`"
 fi
 }
 
+
+
 while true
-	do
-	munu
-	read -e -p "输入菜单编号选择安装：" inst
-	case $inst in
-		1)
-		py3
-		asb
-		;;
-		2)
-		py3
-		;;
-		3)
-		asb
-		;;
-		4)
-		exit
-		;;
-		*)
-		echo "输入错误"
-	esac
+do
+FUNC_MUNU
+menu=`echo -e "\e[1;42m输入菜单编号选择安装：: \e[0m"`
+read -e -p "$menu" inst
+case $inst in
+1)
+	FUNC_PY3
+	FUNC_MYCLI
+	FUNC_ANSIBLE
+	;;
+2)
+	FUNC_PY3
+	;;
+3)
+	FUNC_ANSIBLE
+	;;
+4)
+	FUNC_K9S
+	;;
+5)
+	FUNC_MYCLI
+	;;	
+0)
+	exit
+	;;
+*)
+	FUNC_ECHO_ERROR "输入错误"
+esac
 done
+
